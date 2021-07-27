72d99d583935c48e54216d1749279c1974bb0cb9
Rodolpho Alves <Lyulie@github.com>
2021-07-27 16:11:19 -0300

    setup

---
+++ src/test/java/com/zup/rodolpho/RodolphoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.rodolpho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodolphoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
